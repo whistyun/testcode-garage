d9c56852be0c32f340a9dec1b4044b5bc6f79edf
whistyun <kanadische_goldrute@yahoo.co.jp>
2022-11-05 19:32:40 +0900

    test

--- printenv.py
+++ printenv.py
@@ -1,11 +1,11 @@
 import os
 
 if "Path" in os.environ:
-    print("Path Exists!")
+    print("Path Exists!: "+os.environ["Path"])
 else:
     print("Path Not exists...")
  
 if "TST_VAR" in os.environ:
-    print("TST_VER Exists!")
+    print("TST_VER Exists!: "+os.environ["TST_VAR"])
 else:
     print("TST_VER Not exists...")
