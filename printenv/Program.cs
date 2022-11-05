030ed7fa5b6190c721ed89a6d9761a8118b3dbcd
whistyun <kanadische_goldrute@yahoo.co.jp>
2022-11-05 20:47:25 +0900

    test3

---
+++ printenv/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+#if TstVar
+Console.WriteLine("TST_VAR Exists!");
+#endif
+
+#if PathVar
+Console.WriteLine("PathVar Exists!");
+#endif
