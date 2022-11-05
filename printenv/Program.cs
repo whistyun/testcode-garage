38c263810f5d4e6c5ff9c1bf794a0164b3b3d4e8
whistyun <kanadische_goldrute@yahoo.co.jp>
2022-11-05 23:08:05 +0900

    test4

--- printenv/Program.cs
+++ printenv/Program.cs
@@ -4,6 +4,16 @@
 Console.WriteLine("TST_VAR Exists!");
 #endif
 
+#if GoVar
+Console.WriteLine("GoVar Exists!");
+#endif
+
+var txtVarVal = Environment.GetEnvironmentVariable("TST_VAR");
+if (!string.IsNullOrEmpty(txtVarVal))
+{
+    Console.WriteLine("TST_VAR has value!");
+}
+
 #if PathVar
 Console.WriteLine("PathVar Exists!");
 #endif
